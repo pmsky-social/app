22b5a7b82ade8af0597729182aca928e6c9cd239
drewmcarthur <drewmcarthur1@gmail.com>
2025-02-16 20:33:01 -0700

    split error paragraphs

--- src/errors.ts
+++ src/errors.ts
@@ -12,11 +12,11 @@ export class HandleNotWhitelisted extends Error {
   }
 
   render(): Hole {
-    return html`<p class="error visible">
-      Error: <i>${this.message}.</i>
-      <a href="https://docs.pmsky.social/getting-started/request-access"
-        >Learn how to request access.</a
-      >
-    </p>`;
+    return html`<p class="error visible">Error: <i>${this.message}.</i></p>
+      <p class="error visible">
+        <a href="https://docs.pmsky.social/getting-started/request-access"
+          >Learn how to request access.</a
+        >
+      </p>`;
   }
 }
