e44f0c9c930490108caad2a241f7c5b959fb7de8
drewmcarthur <drewmcarthur1@gmail.com>
2025-02-25 16:59:16 -0700

    nit: spacing

--- src/errors.ts
+++ src/errors.ts
@@ -13,8 +13,8 @@ export class HandleNotWhitelisted extends Error {
 
   render(): Hole {
     return html`<p class="error visible" title=${`Handle: ${this.handle}`}>
-      Error: <i>${this.message}.</i
-      ><a href="https://docs.pmsky.social/getting-started/request-access"
+      Error: <i>${this.message}.</i>
+      <a href="https://docs.pmsky.social/getting-started/request-access"
         >Learn how to request access.</a
       >
     </p>`;
