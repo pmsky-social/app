ab0d2437b8d856889d510fa7c75b556bbf530691
drewmcarthur <drewmcarthur1@gmail.com>
2025-02-26 17:04:18 -0700

    consolidate / refactor single proposal view queries

--- src/db/repos/proposalsRepository.ts
+++ src/db/repos/proposalsRepository.ts
@@ -1,21 +1,40 @@
 import { AppContext } from "#/index";
 import { FeedProposal, feedProposalFromDB } from "#/views/pages/home";
 import { sql } from "kysely";
-import { ProposalType } from "../types";
+import { Proposal, ProposalType } from "../types";
 
 export class ProposalsRepository {
   constructor(private ctx: AppContext) {}
 
+  async getProposal(rkey: string): Promise<Proposal | undefined> {
+    return await this.ctx.db
+      .selectFrom("proposals")
+      .selectAll()
+      .where("rkey", "=", rkey)
+      .executeTakeFirst();
+  }
+
+  async getHydratedProposal(
+    userDid: string,
+    rkey: string
+  ): Promise<FeedProposal | undefined> {
+    const proposals = await this.getProposals(userDid, undefined, 1, 0, rkey);
+    if (proposals.length !== 1) {
+      this.ctx.logger.warn({ proposals, rkey }, "expected 1 proposal for rkey");
+    }
+    return proposals[0];
+  }
+
   async getProposals(
     userDid: string,
-    proposalType: ProposalType = ProposalType.POST_LABEL,
+    proposalType: ProposalType | undefined,
     pageSize: number = 10,
-    page: number = 0
+    page: number = 0,
+    rkey?: string | undefined
   ): Promise<FeedProposal[]> {
     const offset = page * pageSize;
-    return await this.ctx.db
+    let query = this.ctx.db
       .selectFrom("proposals as p")
-      .where("p.type", "=", proposalType)
       .orderBy("p.createdAt", "desc")
       .leftJoin("posts", "posts.uri", "p.subject")
       .leftJoin("proposal_votes", "proposal_votes.subject", "p.uri")
@@ -41,7 +60,16 @@ export class ProposalsRepository {
             AND uv.src = ${userDid}
         ) THEN true ELSE false END
       `.as("alreadyVoted"),
-      ])
+      ]);
+
+    if (proposalType) {
+      query = query.where("p.type", "=", proposalType);
+    }
+    if (rkey) {
+      query = query.where("p.rkey", "=", rkey);
+    }
+
+    return await query
       .execute()
       .then(
         async (rows) =>
